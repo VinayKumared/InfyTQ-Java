70cb6eb30654905f996e53804e28fafe804369f8
VinayKumared <110547760+VinayKumared@users.noreply.github.com>
2022-08-03 19:44:23 +0530

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,13 @@
+class Tester {
+	public static void main(String[] args) {
+		// Implement your code here 
+		//Scanner scan = new Scanner(System.in);
+		//int N = scan.nextInt();
+		int n=8;
+		int j=1;
+		for(int i=1;i<=n;i++){
+		    System.out.print(j+" ");
+		    j=j*2;
+		}
+	}
+}
